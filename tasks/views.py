25ae14534f247b90995e9780acd8359f3311f982
Abdullah-a-omar <Abdullah.dev.omar@gmail.com>
2026-08-25 12:25:15 -0700

    Add filtering, search capabilities, and Swagger documentation

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Task
 from .serializers import TaskSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['completed']
+    search_fields = ['title', 'description']
